edc15d7e5959c0287684124ad186d8c440dec7e1
Etienne Pot <epot@google.com>
2023-07-20 02:14:48 -0700

    Open source the docs folder
    PiperOrigin-RevId: 549566337

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Copyright 2023 The kauldron Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generate documentation.
+
+Usage (from the root directory):
+
+```sh
+pip install -e .[docs]
+
+sphinx-build -b html docs/ docs/_build
+```
+"""
+
+import apitree
+
+apitree.make_project(
+    modules=apitree.ModuleInfo(
+        api='kauldron.kd',
+        module_name='kauldron',  # What to include
+        alias='kd',
+    ),
+    includes_paths={
+        'kauldron/konfig/README.md': 'konfig.md',
+        'kauldron/klinen/README.md': 'klinen.md',
+    },
+    globals=globals(),
+)
---
+++ docs/index.md
@@ -0,0 +1,34 @@
+<!-- mdformat off(myst not supported) -->
+
+```{include} ../README.md
+```
+
+
+<!--
+
+TODO(epot): Include colabs as docs ?
+Like: https://visu3d.readthedocs.io/en/latest/intro.html
+
+-->
+
+```{toctree}
+:hidden:
+:caption: Guides
+
+konfig
+klinen
+```
+
+```{toctree}
+:hidden:
+:caption: Links
+
+GitHub <https://github.com/google-research/kauldron>
+```
+
+```{toctree}
+:hidden:
+:caption: API
+
+api/kd/index
+```
