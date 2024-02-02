ae3a5b4694b427ab87cfea1c331dfcb6bcbed566
Etienne Pot <epot@google.com>
2024-02-02 07:54:10 -0800

    Fix citc info
    PiperOrigin-RevId: 603686459

--- kauldron/xm/_src/experiment.py
+++ kauldron/xm/_src/experiment.py
@@ -20,11 +20,13 @@ from collections.abc import Iterator
 import contextlib
 import dataclasses
 import functools
+import os
 import typing
 from typing import Optional
 
 from etils import epy
 from etils import exm
+from etils import g3_utils
 from kauldron.xm._src import dir_utils
 from kauldron.xm._src import job_lib
 from kauldron.xm._src import job_params
@@ -181,6 +183,14 @@ class Experiment(job_params.JobParams):
     # TODO(epot): Have some heuristic to auto-set the Experiment name from the
     # target ?
     name = self.name or DEFAULT_EXPERIMENT_NAME
+
+    # On Colab, set the g3 dir to the CitC source
+    if epy.is_notebook():
+      citc_info = g3_utils.citc_info_from_source_or_piper(self.citc_source)
+      citc_info = citc_info.immutable()
+      g3_dir = citc_info.g3_dir.replace("/google/src", "/google_src", 1)
+
+      os.environ["BUILD_WORKSPACE_DIRECTORY"] = g3_dir
 
     # Set experiment-level options
     with xm_abc.create_experiment(
