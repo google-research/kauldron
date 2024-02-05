1d721c4ef67c0645323d3331f423db5edb3cc6f3
Etienne Pot <epot@google.com>
2024-02-05 03:44:33 -0800

    Support `.` in keys
    PiperOrigin-RevId: 604269219

--- kauldron/konfig/configdict_base.py
+++ kauldron/konfig/configdict_base.py
@@ -67,12 +67,74 @@ class ConfigDict(ml_collections.ConfigDict):
 
   def __getitem__(self, key: str | int) -> Any:
     key = self._normalize_arg_key(key)
-    return super().__getitem__(key)
+    # TODO(epot): Use `super().__setitem__` after there's an option to disable
+    # `.` checking !!
+    # return super().__setitem__(key, value)
+    # return super().__getitem__(key)
+    return self.__super_getitem__(key)
+
+  def __super_getitem__(self, key: str):
+    from ml_collections.config_dict import config_dict  # pylint: disable=g-import-not-at-top
+
+    try:
+      field = self._fields[key]
+      if isinstance(field, config_dict.FieldReference):
+        return field.get()
+      else:
+        return field
+    except KeyError as e:
+      raise KeyError(self._generate_did_you_mean_message(key, str(e))) from None
 
   def __setitem__(self, key: str | int, value: Any) -> None:
     key = self._normalize_arg_key(key, can_append=True)
     value = _normalize_config_only_value(value, key)
-    return super().__setitem__(key, value)
+    # TODO(epot): Use `super().__setitem__` after there's an option to disable
+    # `.` checking !!
+    # return super().__setitem__(key, value)
+    return self.__super_setitem__(key, value)
+
+  def __super_setitem__(self, key, value):
+    from ml_collections.config_dict import config_dict  # pylint: disable=g-import-not-at-top
+
+    if self.is_locked and key not in self._fields:
+      error_msg = (
+          'Key "{}" does not exist and cannot be added since the '
+          'config is locked. Other fields present: "{}"'
+      )
+      raise KeyError(
+          self._generate_did_you_mean_message(
+              key, error_msg.format(key, self._fields)
+          )
+      )
+
+    if key in self._fields:
+      field = self._fields[key]
+      try:
+        if isinstance(field, config_dict.FieldReference):
+          field.set(value, self._type_safe)
+          return
+        # Skip type checking if the value is a FieldReference of the same type.
+        if not isinstance(
+            value, config_dict.FieldReference
+        ) or value.get_type() is not type(field):
+          if isinstance(value, dict) and self._convert_dict:
+            value = type(self)(value, self._type_safe)
+          value = config_dict._safe_cast(value, type(field), self._type_safe)  # pylint: disable=protected-access
+      except TypeError as e:
+        raise TypeError(
+            "Could not override field '{}' (reference). {}".format(key, str(e))
+        ) from e
+
+    if isinstance(value, dict) and self._convert_dict:
+      value = ConfigDict(value, self._type_safe)
+    elif isinstance(value, config_dict.FieldReference):
+      # TODO(sergomez): We should consider using value.get_type().
+      ref_type = config_dict._NoneType if value.empty() else type(value.get())  # pylint: disable=protected-access
+      if ref_type is dict or ref_type is config_dict.FrozenConfigDict:
+        value_cd = ConfigDict(value.get(), self._type_safe)
+        value.set(value_cd, False)
+
+    self._fields[key] = value
 
   def __repr__(self) -> str:
     visited = _VisitedTracker()
@@ -95,6 +157,9 @@ class ConfigDict(ml_collections.ConfigDict):
       if not (0 <= key < num_args + can_append):
         raise IndexError(f'argument index {key} is not smaller than {num_args}')
       key = str(key)
+    elif isinstance(key, tuple):
+      # In the CLI: `--cfg.args['a.b']` is parsed as `('args', ('a', 'b')))`
+      key = '.'.join(key)
     return key
 
   def to_json(self, **dumps_kwargs) -> str:  # pytype: disable=signature-mismatch
@@ -266,10 +331,10 @@ class _ListVisitor(_Visitor):
 
   CLS = (list, tuple)
 
-  def _recurse(self, obj: list[Any] | tuple[Any]) -> Any:
+  def _recurse(self, obj: list[Any] | tuple[Any, ...]) -> Any:
     return [_Repr(self.recurse(v)) for v in obj]
 
-  def _repr(self, obj: list[Any] | tuple[Any]) -> str:
+  def _repr(self, obj: list[Any] | tuple[Any, ...]) -> str:
     return epy.Lines.make_block(
         content=self._recurse(obj),
         braces='[' if isinstance(obj, list) else '(',
