238a2be82f4f7d9683adc0b2933c03d2cba22e9a
The kauldron Authors <no-reply@google.com>
2024-02-27 16:14:40 -0800

    Temporarily unify TAP-Vid evaluation metric for both TAPIR and VideoSRT.
    PiperOrigin-RevId: 610907929

--- kauldron/metrics/point_tracking.py
+++ kauldron/metrics/point_tracking.py
@@ -12,13 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Evaluation metrics for point tracking."""
+"""Evaluation metrics for point tracking. TODO(yiya) move to contrib."""
 
 from __future__ import annotations
 
 from collections.abc import Sequence
 import dataclasses
+from typing import Literal, Optional
 
+import einops
 import flax
 import jax.numpy as jnp
 from kauldron import kontext
@@ -27,8 +29,8 @@ from kauldron.typing import Float, Shape, typechecked  # pylint: disable=g-multi
 
 
 def get_evaluation_frames(
-    query_points: Float["*B Q 3"], num_frames: int, query_mode: str
-) -> Float["*B T"]:
+    query_frame: Float["*B Q"], num_frames: int, query_mode: str
+) -> Float["*B Q T"]:
   """Get which frames to evaluate for point tracking."""
   eye = jnp.eye(num_frames, dtype=jnp.int32)
   if query_mode == "first":
@@ -40,7 +42,6 @@ def get_evaluation_frames(
   else:
     raise ValueError("Unknown query mode " + query_mode)
 
-  query_frame = query_points[..., 0]
   query_frame = jnp.round(query_frame).astype(jnp.int32)
   evaluation_frames = query_frame_to_eval_frames[query_frame] > 0
   return evaluation_frames
@@ -50,10 +51,13 @@ def get_evaluation_frames(
 class TapOcclusionAccuracy(metrics.Metric):
   """Occlusion accuracy for point tracking."""
 
-  query_points: kontext.Key = kontext.REQUIRED  # e.g. "batch.query_points"
-  gt_visible: kontext.Key = kontext.REQUIRED  # e.g. "batch.visible"
   pred_visible: kontext.Key = kontext.REQUIRED  # e.g. "pred.visible"
+  gt_visible: kontext.Key = kontext.REQUIRED  # e.g. "batch.visible"
+  query_frame: Optional[kontext.Key] = None  # e.g. "batch.query_frame"
+  query_mask: Optional[kontext.Key] = None  # e.g. "batch.query_mask"
   query_mode: str  # e.g. "first" or "strided"
+  axis_order: Literal["BQTC", "BTQC"] = "BQTC"  # TODO(yiya) remove this
+  pred_is_logits: bool = False  # TODO(yiya) remove this
 
   @flax.struct.dataclass
   class State(metrics.AverageState):
@@ -62,22 +66,39 @@ class TapOcclusionAccuracy(metrics.Metric):
   @typechecked
   def get_state(
       self,
-      query_points: Float["*B Q 3"],
-      gt_visible: Float["*B Q T 1"],
-      pred_visible: Float["*B Q T 1"],
+      pred_visible: Float["*B M N 1"],
+      gt_visible: Float["*B M N 1"],
+      query_frame: Optional[Float["*B Q"]] = None,
+      query_mask: Optional[Float["*B Q"]] = None,
   ) -> TapOcclusionAccuracy.State:
+    if self.pred_is_logits:
+      pred_visible = pred_visible.squeeze(-1) > 0
+    else:
+      pred_visible = pred_visible.squeeze(-1).astype(jnp.bool_)
     gt_visible = gt_visible.squeeze(-1).astype(jnp.bool_)
-    pred_visible = pred_visible.squeeze(-1).astype(jnp.bool_)
 
-    evaluation_frames = get_evaluation_frames(
-        query_points, Shape("T")[0], self.query_mode
-    )
+    if query_frame is None:  # Assume query from first frame if not provided
+      query_frame = jnp.zeros(Shape("*B Q"))
+
+    if self.axis_order == "BTQC":
+      pred_visible = einops.rearrange(pred_visible, "... M N -> ... N M")
+      gt_visible = einops.rearrange(gt_visible, "... M N -> ... N M")
+      evaluation_frames = get_evaluation_frames(
+          query_frame, Shape("M")[0], self.query_mode
+      )
+    else:
+      evaluation_frames = get_evaluation_frames(
+          query_frame, Shape("N")[0], self.query_mode)
 
     # Occlusion accuracy is simply how often the predicted occlusion equals the
     # ground truth.
-    values = jnp.sum(
-        jnp.equal(pred_visible, gt_visible) & evaluation_frames, axis=(-2, -1)
-    ) / jnp.sum(evaluation_frames, axis=(-2, -1))
+    values = jnp.equal(pred_visible, gt_visible) & evaluation_frames
+    values = values.sum(axis=-1)
+    count = evaluation_frames.sum(axis=-1)
+    if query_mask is not None:
+      values = values * query_mask
+      count = count * query_mask
+    values = values.sum(axis=-1) / (count.sum(axis=-1) + 1e-8)
 
     return self.State.from_values(values=values)
 
@@ -86,11 +107,13 @@ class TapOcclusionAccuracy(metrics.Metric):
 class TapPositionAccuracy(metrics.Metric):
   """Position accuracy for visible points only."""
 
-  query_points: kontext.Key = kontext.REQUIRED  # e.g. "batch.query_points"
-  gt_visible: kontext.Key = kontext.REQUIRED  # e.g. "batch.visible"
-  gt_tracks: kontext.Key = kontext.REQUIRED  # e.g. "batch.target_points"
   pred_tracks: kontext.Key = kontext.REQUIRED  # e.g. "pred.tracks"
+  gt_tracks: kontext.Key = kontext.REQUIRED  # e.g. "batch.target_points"
+  gt_visible: kontext.Key = kontext.REQUIRED  # e.g. "batch.visible"
+  query_frame: Optional[kontext.Key] = None  # e.g. "batch.query_frame"
+  query_mask: Optional[kontext.Key] = None  # e.g. "batch.query_mask"
   query_mode: str  # e.g. "first" or "strided"
+  axis_order: Literal["BQTC", "BTQC"] = "BQTC"  # TODO(yiya) remove this
   # pixel radius to compute accuracy
   thresholds: Sequence[int] = (1, 2, 4, 8, 16)
 
@@ -101,16 +124,28 @@ class TapPositionAccuracy(metrics.Metric):
   @typechecked
   def get_state(
       self,
-      query_points: Float["*B Q 3"],
-      gt_visible: Float["*B Q T 1"],
-      gt_tracks: Float["*B Q T 2"],
-      pred_tracks: Float["*B Q T 2"],
+      pred_tracks: Float["*B M N 2"],
+      gt_tracks: Float["*B M N 2"],
+      gt_visible: Float["*B M N 1"],
+      query_frame: Optional[Float["*B Q"]] = None,
+      query_mask: Optional[Float["*B Q"]] = None,
   ) -> TapPositionAccuracy.State:
     gt_visible = gt_visible.squeeze(-1).astype(jnp.bool_)
 
-    evaluation_frames = get_evaluation_frames(
-        query_points, Shape("T")[0], self.query_mode
-    )
+    if query_frame is None:  # Assume query from first frame if not provided
+      query_frame = jnp.zeros(Shape("*B Q"))
+
+    if self.axis_order == "BTQC":
+      pred_tracks = einops.rearrange(pred_tracks, "... M N C -> ... N M C")
+      gt_tracks = einops.rearrange(gt_tracks, "... M N C -> ... N M C")
+      gt_visible = einops.rearrange(gt_visible, "... M N -> ... N M")
+      evaluation_frames = get_evaluation_frames(
+          query_frame, Shape("M")[0], self.query_mode
+      )
+    else:
+      evaluation_frames = get_evaluation_frames(
+          query_frame, Shape("N")[0], self.query_mode
+      )
 
     all_frac_within = []
     for thresh in self.thresholds:
@@ -124,11 +159,14 @@ class TapPositionAccuracy(metrics.Metric):
       # Compute the frac_within_threshold, which is the fraction of points
       # within the threshold among points that are visible in the ground truth,
       # ignoring whether they're predicted to be visible.
-      count_correct = jnp.sum(is_correct & evaluation_frames, axis=(-2, -1))
-      count_visible_points = jnp.sum(
-          gt_visible & evaluation_frames, axis=(-2, -1)
-      )
-      frac_correct = count_correct / count_visible_points
+      count_correct = jnp.sum(is_correct & evaluation_frames, axis=-1)
+      count_visible_points = jnp.sum(gt_visible & evaluation_frames, axis=-1)
+      if query_mask is not None:
+        count_correct = count_correct * query_mask
+        count_visible_points = count_visible_points * query_mask
+      count_correct = count_correct.sum(axis=-1)
+      count_visible_points = count_visible_points.sum(axis=-1)
+      frac_correct = count_correct / (count_visible_points + 1e-8)
       all_frac_within.append(frac_correct)
 
     values = jnp.mean(jnp.stack(all_frac_within, axis=-1), axis=-1)
@@ -140,12 +178,15 @@ class TapPositionAccuracy(metrics.Metric):
 class TapAverageJaccard(metrics.Metric):
   """Average Jaccard considering both location and occlusion accuracy."""
 
-  query_points: kontext.Key = kontext.REQUIRED  # e.g. "batch.query_points"
-  gt_visible: kontext.Key = kontext.REQUIRED  # e.g. "batch.visible"
-  gt_tracks: kontext.Key = kontext.REQUIRED  # e.g. "batch.target_points"
-  pred_visible: kontext.Key = kontext.REQUIRED  # e.g. "pred.visible"
   pred_tracks: kontext.Key = kontext.REQUIRED  # e.g. "pred.tracks"
+  pred_visible: kontext.Key = kontext.REQUIRED  # e.g. "pred.visible"
+  gt_tracks: kontext.Key = kontext.REQUIRED  # e.g. "batch.target_points"
+  gt_visible: kontext.Key = kontext.REQUIRED  # e.g. "batch.visible"
+  query_frame: Optional[kontext.Key] = None  # e.g. "batch.query_frame"
+  query_mask: Optional[kontext.Key] = None  # e.g. "batch.query_mask"
   query_mode: str  # e.g. "first" or "strided"
+  axis_order: Literal["BQTC", "BTQC"] = "BQTC"  # TODO(yiya) remove this
+  pred_is_logits: bool = False  # TODO(yiya) remove this
   # pixel radius to compute accuracy
   thresholds: Sequence[int] = (1, 2, 4, 8, 16)
 
@@ -156,18 +197,34 @@ class TapAverageJaccard(metrics.Metric):
   @typechecked
   def get_state(
       self,
-      query_points: Float["*B Q 3"],
-      gt_visible: Float["*B Q T 1"],
-      gt_tracks: Float["*B Q T 2"],
-      pred_visible: Float["*B Q T 1"],
-      pred_tracks: Float["*B Q T 2"],
+      pred_tracks: Float["*B M N 2"],
+      pred_visible: Float["*B M N 1"],
+      gt_tracks: Float["*B M N 2"],
+      gt_visible: Float["*B M N 1"],
+      query_frame: Optional[Float["*B Q"]] = None,
+      query_mask: Optional[Float["*B Q"]] = None,
   ) -> TapAverageJaccard.State:
+    if self.pred_is_logits:
+      pred_visible = pred_visible.squeeze(-1) > 0
+    else:
+      pred_visible = pred_visible.squeeze(-1).astype(jnp.bool_)
     gt_visible = gt_visible.squeeze(-1).astype(jnp.bool_)
-    pred_visible = pred_visible.squeeze(-1).astype(jnp.bool_)
 
-    evaluation_frames = get_evaluation_frames(
-        query_points, Shape("T")[0], self.query_mode
-    )
+    if query_frame is None:  # Assume query from first frame if not provided
+      query_frame = jnp.zeros(Shape("*B Q"))
+
+    if self.axis_order == "BTQC":
+      pred_tracks = einops.rearrange(pred_tracks, "... M N C -> ... N M C")
+      pred_visible = einops.rearrange(pred_visible, "... M N -> ... N M")
+      gt_tracks = einops.rearrange(gt_tracks, "... M N C -> ... N M C")
+      gt_visible = einops.rearrange(gt_visible, "... M N -> ... N M")
+      evaluation_frames = get_evaluation_frames(
+          query_frame, Shape("M")[0], self.query_mode
+      )
+    else:
+      evaluation_frames = get_evaluation_frames(
+          query_frame, Shape("N")[0], self.query_mode
+      )
 
     all_jaccard = []
     for thresh in self.thresholds:
@@ -179,7 +236,7 @@ class TapAverageJaccard(metrics.Metric):
       is_correct = jnp.logical_and(within_dist, gt_visible)
 
       true_positives = jnp.sum(
-          is_correct & pred_visible & evaluation_frames, axis=(-2, -1)
+          is_correct & pred_visible & evaluation_frames, axis=-1
       )
 
       # The denominator of the Jaccard metric is the true positives plus
@@ -191,13 +248,18 @@ class TapAverageJaccard(metrics.Metric):
       #
       # False positives are simply points that are predicted to be visible,
       # but the ground truth is not visible or too far from the prediction.
-      gt_positives = jnp.sum(gt_visible & evaluation_frames, axis=(-2, -1))
+      gt_positives = jnp.sum(gt_visible & evaluation_frames, axis=-1)
       false_positives = (~gt_visible) & pred_visible
       false_positives = false_positives | ((~within_dist) & pred_visible)
-      false_positives = jnp.sum(
-          false_positives & evaluation_frames, axis=(-2, -1)
-      )
-      jaccard = true_positives / (gt_positives + false_positives)
+      false_positives = jnp.sum(false_positives & evaluation_frames, axis=-1)
+      if query_mask is not None:
+        true_positives = true_positives * query_mask
+        gt_positives = gt_positives * query_mask
+        false_positives = false_positives * query_mask
+      true_positives = true_positives.sum(axis=-1)
+      gt_positives = gt_positives.sum(axis=-1)
+      false_positives = false_positives.sum(axis=-1)
+      jaccard = true_positives / (gt_positives + false_positives + 1e-8)
       all_jaccard.append(jaccard)
 
     values = jnp.mean(jnp.stack(all_jaccard, axis=-1), axis=-1)
