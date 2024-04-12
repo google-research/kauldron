798c7842568f113625459169a9c737b3f08beaf2
Etienne Pot <epot@google.com>
2024-04-12 01:24:56 -0700

    Use builder-less version of TFDS
    PiperOrigin-RevId: 624086459

--- kauldron/data/loaders/seqio.py
+++ kauldron/data/loaders/seqio.py
@@ -18,12 +18,18 @@
 import dataclasses
 from typing import Any, Callable, Mapping, Optional
 
+from etils import epy
 from grain._src.core import constants
 import jax
 from kauldron.data.loaders import base
-from kauldron.typing import PRNGKey, PRNGKeyLike  # pylint: disable=g-multiple-import
-import seqio
+from kauldron.typing import PRNGKey, PRNGKeyLike  # pylint: disable=g-multiple-import,g-importing-member
 import tensorflow as tf
+
+
+with epy.lazy_imports(
+    error_callback=lambda _: "seqio require to add `//third_party/py/seqio` to your trainer."
+):
+  import seqio  # pylint: disable=g-import-not-at-top  # pytype: disable=import-error
 
 
 def _get_seqio_task_dataset(
