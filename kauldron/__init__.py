1749ca79d7aa17ac0a4ca8e384fde579d0651b81
Klaus Greff <klausg@google.com>
2024-04-19 05:23:31 -0700

    Catch `import kauldron as kd` vs `from kauldron import kd`
    PiperOrigin-RevId: 626335030

--- kauldron/__init__.py
+++ kauldron/__init__.py
@@ -22,3 +22,13 @@
 # A new PyPI release will be pushed everytime `__version__` is increased
 # When changing this, also update the CHANGELOG.md
 __version__ = '0.1.0'
+
+
+def __getattr__(name):  # pylint: disable=invalid-name
+  """Any attribute access apart from `__version__` should raise an error."""
+  if name == '__version__':
+    return __version__
+  raise AttributeError(
+      'Please always use "from kauldron import kd", '
+      'never "import kauldron as kd".'
+  )
