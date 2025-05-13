google-research/kauldron

commit 3d65da6d0e7ca5d4331983964f8a2e211eeff5de

Use element_spec from eval_ds during evaluation if available.
To avoid creating the train_ds in evaluation jobs, we first check if there is an evaluation dataset that we can use to get the element_spec. If not, we fall back to using the element_spec from train_ds.

PiperOrigin-RevId: 755337463

kauldron/evals/eval_impl.py
@@ -22,6 +22,7 @@ import dataclasses
 import hashlib
 
 from absl import logging
+from etils import enp
 from etils import epath
 from kauldron.checkpoints import checkpointer
 from kauldron.checkpoints import partial_loader
@@ -30,6 +31,7 @@ from kauldron.evals import run_strategies
 from kauldron.train import auxiliaries
 from kauldron.train import train_step
 from kauldron.train import trainer_lib
+from kauldron.typing import PyTree  # pylint: disable=g-importing-member
 from kauldron.utils.status_utils import status  # pylint: disable=g-importing-member
 import orbax.checkpoint as ocp
 
@@ -75,6 +77,7 @@ def continuous_eval(
       skip_optimizer=all(
           trainer.evals[name].discard_opt for name in eval_names
       ),
+      element_spec=_get_element_spec(trainer, eval_names),
   )
   aux = {eval_name: auxiliaries.AuxiliariesState() for eval_name in eval_names}
 
@@ -261,3 +264,16 @@ def _get_eval_ckpt(
     )
   else:
     raise ValueError(f'Unsupported checkpointer type: {type(trainer_ckpt)}')
+
+
+def _get_element_spec(
+    trainer: trainer_lib.Trainer, eval_names: list[str]
+) -> PyTree[enp.ArraySpec]:
+  """Returns the element spec from eval_ds if available, else from train_ds."""
+  if trainer.eval_ds is not None:
+    return trainer.eval_ds.element_spec
+  for name in eval_names:
+    ev = trainer.evals[name]
+    if isinstance(ev, evaluators_lib.Evaluator):
+      return ev.ds.element_spec
+  return trainer.train_ds.element_spec

kauldron/train/trainer_lib.py
@@ -23,6 +23,7 @@ import typing
 from typing import Any, Optional, Self
 
 from etils import edc
+from etils import enp
 from etils import epath
 import flax
 from flax import linen as nn
@@ -44,6 +45,7 @@ from kauldron.train import rngs_lib
 from kauldron.train import setup_utils
 from kauldron.train import train_loop
 from kauldron.train import train_step
+from kauldron.typing import PyTree  # pylint: disable=g-importing-member
 from kauldron.utils import _jax
 from kauldron.utils import chrono_utils
 from kauldron.utils import config_util
@@ -349,11 +351,17 @@ class Trainer(config_util.BaseConfig):
 
   # Do not use property to make it explicit this is recomputed each time
   def init_state(
-      self, *, skip_transforms: bool = False, skip_optimizer: bool = False
+      self,
+      *,
+      skip_transforms: bool = False,
+      skip_optimizer: bool = False,
+      element_spec: PyTree[enp.ArraySpec] | None = None,
   ) -> train_step.TrainState:
-    """Create the state: `cfg.trainstep.init(cfg.train_ds.element_spec)`."""
+    """Create the state: `cfg.trainstep.init(element_spec)`."""
+    if element_spec is None:
+      element_spec = self.train_ds.element_spec
     return self.trainstep.init(
-        self.train_ds.element_spec,
+        element_spec,
         skip_transforms=skip_transforms,
         skip_optimizer=skip_optimizer,
     )

commit a1d087e2676254c87d1079626c587135c4577b85

Load element_spec from disk in evaluation.
https://github.com/google-research/kauldron/commit/3d65da6d0e7ca5d4331983964f8a2e211eeff5de (using the element_spec from eval_ds in evaluation) caused problems in usecases where the batches in evaluation look differently than those in training. Instead, we now load the element_spec that is already dumped during training.

PiperOrigin-RevId: 758220126

kauldron/evals/eval_impl.py
@@ -20,18 +20,23 @@ from collections.abc import Iterator
 import contextlib
 import dataclasses
 import hashlib
+import json
+import time
 
 from absl import logging
 from etils import enp
 from etils import epath
+from kauldron import data
 from kauldron.checkpoints import checkpointer
 from kauldron.checkpoints import partial_loader
+from kauldron.data import utils as data_utils
 from kauldron.evals import evaluators as evaluators_lib
 from kauldron.evals import run_strategies
 from kauldron.train import auxiliaries
 from kauldron.train import train_step
 from kauldron.train import trainer_lib
 from kauldron.typing import PyTree  # pylint: disable=g-importing-member
+from kauldron.utils import constants
 from kauldron.utils.status_utils import status  # pylint: disable=g-importing-member
 import orbax.checkpoint as ocp
 
@@ -77,7 +82,7 @@ def continuous_eval(
       skip_optimizer=all(
           trainer.evals[name].discard_opt for name in eval_names
       ),
-      element_spec=_get_element_spec(trainer, eval_names),
+      element_spec=_get_element_spec(trainer),
   )
   aux = {eval_name: auxiliaries.AuxiliariesState() for eval_name in eval_names}
 
@@ -266,14 +271,16 @@ def _get_eval_ckpt(
     raise ValueError(f'Unsupported checkpointer type: {type(trainer_ckpt)}')
 
 
-def _get_element_spec(
-    trainer: trainer_lib.Trainer, eval_names: list[str]
-) -> PyTree[enp.ArraySpec]:
-  """Returns the element spec from eval_ds if available, else from train_ds."""
-  if trainer.eval_ds is not None:
-    return trainer.eval_ds.element_spec
-  for name in eval_names:
-    ev = trainer.evals[name]
-    if isinstance(ev, evaluators_lib.Evaluator):
-      return ev.ds.element_spec
-  return trainer.train_ds.element_spec
+def _get_element_spec(trainer: trainer_lib.Trainer) -> PyTree[enp.ArraySpec]:
+  """Loads the element spec from disk, or from train_ds for eval-only jobs."""
+
+  # Load from training
+  path = trainer.workdir / constants.ELEMENT_SPEC_FILENAME
+  sleep_time = 10
+  while not path.exists():
+    logging.info(
+        'Waiting for element_spec in %s. Sleeping for %ds.', path, sleep_time
+    )
+    time.sleep(sleep_time)
+  spec = json.loads(path.read_text())
+  return data_utils.json_to_spec(spec)

kauldron/evals/eval_impl_test.py
@@ -55,6 +55,10 @@ def test_eval_impl(tmp_path: epath.Path):
           _mocked_iterator,
       ),
   ):
+
+    # Write element_spec to disk so eval can use it in initialization
+    trainer.writer.write_element_spec(0, trainer.train_ds.element_spec)
+
     aux = trainer.continuous_eval('eval')
     # Ensure at least one checkpoint was computed
     assert 'recon' in aux['eval'].loss_states