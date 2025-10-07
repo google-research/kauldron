b69031c7a850c672cfa46f0869dada782e2ae434
The kauldron Authors <no-reply@google.com>
2025-10-07 10:55:35 -0700

    This PR aims at simplifying building modular configs. It brings a new possibility of config declaration in config files as follows:
    ```python
    @dataclasses.dataclass(frozen=True, kw_only=True)
    class ConfigArgs:
      """Config for metaconfig demo."""
    
      model: str = "tiny_vit"
      model_variant: str = "ViT-Ti/8"
      dataset_name: str = "mnist"
      dataset_train_split: str = "train"
      dataset_eval_splits: tuple[str, ...] = ("val", "test")
    
    def get_config(self) -> kd.train.Trainer:
      cfg = kd.train.Trainer()
      if self.dataset_name == "mnist":
        cfg.train_ds = ...
      elif self.dataset_name == "imagenet":
        cfg.train_ds = ...
      cfg.num_train_steps = 1
      return cfg
    
    def sweep_dataset():
      # TODO(geco): discuss other options: __init_args__, __build_args__
      yield {"__args__.dataset_name": "mnist"}
      yield {"__args__.dataset_name": "imagenet"}
    ```
    CLI: `xmanager launch path/to/config.py –cfg.__args__.dataset_train_split="override" –cfg.num_train_steps=2`
    
    Note that in this API, the `get_config` is moved inside the `MetaConfig` class.
    
    implementation: instead of directly instantiating the config, simply record flags. Then when a flag value is requested, the config is built on demand.
    PiperOrigin-RevId: 816286743

--- kauldron/konfig/flags_utils.py
+++ kauldron/konfig/flags_utils.py
@@ -14,12 +14,16 @@
 
 """Flags utils."""
 
+# pylint: disable=g-multiple-import
+# pylint: disable=protected-access
+
 from absl import flags
 from kauldron.konfig import configdict_base
-from ml_collections import config_flags
+from kauldron.konfig import module_configdict
+from ml_collections.config_flags import config_flags
 
 
-def DEFINE_config_file(  # pylint: disable=g-bad-name
+def DEFINE_config_file(  # pylint: disable=invalid-name
     name: str,
     default: str | None = None,
     help_string: str = "path to config file.",
@@ -29,8 +33,9 @@ def DEFINE_config_file(  # pylint: disable=g-bad-name
 ) -> flags.FlagHolder[configdict_base.ConfigDict]:
   """Defines flag for `ConfigDict`.
 
-  This is a wrapper over `ml_collections.config_flags.DEFINE_config_file` which
-  set the relevant default params.
+  This is similar to `ml_collections.config_flags.DEFINE_config_file`, but
+  allows for lazy resolution of the config module. The config is resolved from
+  flag values when the `value` property of the defined flag is accessed.
 
   Args:
     name: Flag name, optionally including extra config after a colon.
@@ -39,17 +44,68 @@ def DEFINE_config_file(  # pylint: disable=g-bad-name
       "path to config file.")
     flag_values: FlagValues instance used for parsing. (default:
       absl.flags.FLAGS)
-    lock_config: If set to True, loaded config will be locked through calling
-      .lock() method on its instance (if it exists). (default: False)
+    lock_config: Unused argument that we keep for compatibility.
 
   Returns:
     a handle to defined flag.
   """
-  return config_flags.DEFINE_config_file(
-      name,
-      default,
-      help_string,
+  assert not lock_config, "lock_config is not supported."
+  flag = _LazyConfigFlag(
+      parser=_RecordConfigFileFlagParser(name=name),
+      serializer=flags.ArgumentSerializer(),
+      name=name,
+      default=default,
+      help_string=help_string,
       flag_values=flag_values,
-      lock_config=lock_config,
       accept_new_attributes=True,
   )
+
+  flag_holder = flags.DEFINE_flag(flag, flag_values)
+  return flag_holder
+
+
+class _RecordConfigFileFlagParser(flags.ArgumentParser):
+  """A ConfigFileFlagParser that simply records input arguments.
+
+  It creates a konfig.ModuleConfigDict with the input arguments.
+  """
+
+  def __init__(self, name):
+    self.name = name
+
+  def parse(self, path):
+    """Creates a ModuleConfigDict from a config file path."""
+    if ":" in path:
+      # TODO(geco): add a doc and raise warning that this is the old behavior.
+      module_path, cli_str_arg = path.split(":", 1)
+    else:
+      module_path, cli_str_arg = path, ""
+
+    config_module = config_flags._LoadConfigModule(  # pylint: disable=protected-access
+        "{}_config".format(self.name), module_path
+    )
+    modulecfg = module_configdict.ModuleConfigDict(
+        module=config_module, cli_str_arg=cli_str_arg
+    )
+    return modulecfg
+
+  def flag_type(self):
+    return "config object"
+
+
+class _LazyConfigFlag(config_flags._ConfigFlag):
+  """A ConfigFlag that is lazily resolved.
+
+  The underlying _value field should be a konfig.ModuleConfigDict.
+  """
+
+  @property
+  def value(self):
+    assert isinstance(
+        self._value, module_configdict.ModuleConfigDict
+    ), "The value of the flag is not a ModuleConfigDict."
+    return self._value.module_config
+
+  @value.setter
+  def value(self, val):
+    self._value = val
