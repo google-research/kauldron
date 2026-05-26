f835f68cc2bfd3f9d0861ac9e3ad3aa47cf3f9f0
The kauldron Authors <no-reply@google.com>
2026-05-26 12:41:10 -0700

    Flip default freeze to False in konfig.resolve and remove ImmutableDict.
    PiperOrigin-RevId: 921623842

--- kauldron/konfig/__init__.py
+++ kauldron/konfig/__init__.py
@@ -46,6 +46,7 @@ from kauldron.konfig.configdict_proxy import resolve
 from kauldron.konfig.export_utils import export
 from kauldron.konfig.export_utils import export_qualname
 from kauldron.konfig.fake_import_utils import imports
+from kauldron.konfig import immutabledict_lib
 from kauldron.konfig.fake_import_utils import mock_modules
 from kauldron.konfig.fake_import_utils import set_lazy_imported_modules
 from kauldron.konfig.flags_utils import DEFINE_config_file
